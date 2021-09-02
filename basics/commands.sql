58479f08accb8a4e42f02574e029409e49be0288
Ankit Negi <89020930+serAnkii@users.noreply.github.com>
2021-09-02 10:37:00 +0530

    Update commands.sql

--- basics/commands.sql
+++ basics/commands.sql
@@ -4,66 +4,67 @@ BTW its my assignment also : lolz
 btw this function format is for the easy understanding
 ≧◉◡◉≦
 */
-login(null)
-{   
-//login to the oracle
-//open cmd and type
-c:\sqlplus
-//enter username and the password
-};
+--login(null)
+--{   
+--login to the oracle
+--open cmd and type
+--c:\sqlplus
+--enter username
+--and the password
+--};
 
-viweusername()
-{
-SHOW USER
-}
+--viweusername()
+--{
+show user
+--}
 
-viewdate()
-{
+--viewdate()
+--{
 SELECT sysdate from dual ;
-}
+--}
 
-clearcsr()
-{
+--clearcsr()
+--{
 CLEARSCREEN
-}
+--}
 
-environmentcariables()
-{
+--environmentcariables()
+--{
 show all
-}
+--}
 
-performarthematic()
-{
+--performarthematic()
+--{
 select 45+45 from dual;
-}
+--}
 
-createtablestudent()
-{
+--createtablestudent()
+--{
 create table student(rollno number,name varchar2(10),city varchar2(20));
-}
+--}
 
-displaystructure()
-{
+--displaystructure()
+--{
 describe student;
-}
+--}
 
-seerecords()
-{
+--seerecords()
+--{
 select * from student;
-}
+--}
 
-insertelements()
-{
+--insertelements()
+--{
 insert into student values(1,'Ankit','Dehardun');
 insert into student values(2,'whatever','whatever');
 insert into student values(3,'whatever','whatever');
 insert into student values(4,'whatever','whatever');
 insert into student values(5,'whatever','whatever');
 
-commit;//once it is executed you cannot rollback any any action and it also releases the previous operations
-}
+commit;--once it is executed you cannot rollback any any action and it also releases the previous operations
+--}
 
-addcolumn()
-{
+--addcolumn()
+--{
 
 }
